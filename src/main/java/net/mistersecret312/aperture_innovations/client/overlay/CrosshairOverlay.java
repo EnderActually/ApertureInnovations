be3ce9e65004d98e1961a46d048a385a77865323
mistersecret312 <sergiigamer7@gmail.com>
2026-01-18 00:11:36 +0200

    overlay parity changes

--- src/main/java/net/mistersecret312/aperture_innovations/client/overlay/CrosshairOverlay.java
+++ src/main/java/net/mistersecret312/aperture_innovations/client/overlay/CrosshairOverlay.java
@@ -1,6 +1,5 @@
 package net.mistersecret312.aperture_innovations.client.overlay;
 
-import ca.weblite.objc.Client;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
@@ -9,15 +8,11 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-import net.mistersecret312.aperture_innovations.ApertureInnovations;
 import net.mistersecret312.aperture_innovations.client.ColorUtil;
-import net.mistersecret312.aperture_innovations.events.ClientEvents;
 import net.mistersecret312.aperture_innovations.init.ItemInit;
 import net.mistersecret312.aperture_innovations.items.PortalGunItem;
 import net.mistersecret312.aperture_innovations.portal.ClientPortalLink;
 import net.mistersecret312.aperture_innovations.portal.ClientPortalUtilities;
-import net.mistersecret312.aperture_innovations.portal.PortalLink;
-import net.mistersecret312.aperture_innovations.portal.PortalLinkData;
 
 import static net.mistersecret312.aperture_innovations.client.renderer.PortalRenderer.LINKS;
 
@@ -26,6 +21,11 @@ public class CrosshairOverlay
 	public static final IGuiOverlay OVERLAY = ((gui, guiGraphics, partialTick, screenWidth, screenHeight)
 													   -> {
 			LocalPlayer player = Minecraft.getInstance().player;
+
+			if (Minecraft.getInstance().options.hideGui) return;
+
+			if (!Minecraft.getInstance().options.getCameraType().isFirstPerson()) return;
+
 			if (player == null) return;
 
 			PoseStack poseStack = guiGraphics.pose();
